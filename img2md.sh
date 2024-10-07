3f93e9fd4b03c378f8e561062d2887621422579c
b0llull0s <panicthreads@proton.me>
2024-10-07 16:02:04 +0100

    to the clipboard baby

--- img2md.sh
+++ img2md.sh
@@ -16,4 +16,18 @@ MIME_TYPE=$(file --mime-type -b "$IMAGE_FILE")
 
 BASE64_STRING=$(base64 -w 0 "$IMAGE_FILE")
 
-echo "<img src=\"data:${MIME_TYPE};base64,${BASE64_STRING}\" alt=\"image\" />"
+IMG_TAG="<img src=\"data:${MIME_TYPE};base64,${BASE64_STRING}\" alt=\"image\" />"
+
+echo "$IMG_TAG"
+
+TEMP_FILE=$(mktemp /tmp/img2md.XXXXXX)
+
+echo "$IMG_TAG" > "$TEMP_FILE"
+
+if cliphist store < "$TEMP_FILE"; then
+    echo "HTML <img> tag copied to clipboard!"
+else
+    echo "Failed to copy to clipboard."
+fi
+
+rm "$TEMP_FILE"
